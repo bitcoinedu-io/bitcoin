716a8ae9f52b9b7bef617ab5e7fbfd1286083770
Thomas Lundqvist <tlundqvist@acm.org>
2021-02-22 10:52:58 +0100

    Additional small change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -154,6 +154,7 @@ public:
 
         checkpointData = {
             {
+              { 0, uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")}
             }
         };
 
